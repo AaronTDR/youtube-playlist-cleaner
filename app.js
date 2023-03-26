2f67cb60c656eb54c284e90d9aa41c2299651251
AaronTDR <aaron.tdr12w@gmail.com.com>
2023-03-26 12:42:39 -0600

    Add the variables videos, menuButton, deleteButton

---
+++ app.js
@@ -0,0 +1,10 @@
+// Gets the videos from the list
+const videos = document.querySelectorAll(
+  ".yt-simple-endpoint.style-scope.ytd-playlist-video-renderer"
+);
+// Gets the dropdown menu
+const menuButton = document.querySelector("#contents #menu #button");
+
+const deleteButton = document.querySelector(
+  "tp-yt-iron-dropdown #contentWrapper ytd-menu-popup-renderer #items > :nth-child(3)"
+);
